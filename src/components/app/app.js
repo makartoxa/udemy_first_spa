0f9c04529727622817e1b5e8081ad40da79d5e8b
user2507 <makar.toxa@gmail.com>
2022-09-13 18:58:54 +0300

    add update filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,8 @@ class App extends Component {
                 {name: 'Alex B.', salary: 1500, increase: false, nameStar: false, id: 2},
                 {name: 'Misha R.', salary: 3800, increase: false, nameStar: false, id: 3},
             ],
-            term: ''
+            term: '',
+            filter: 'all'
         }
         this.maxId = 4;
     }
@@ -78,11 +79,26 @@ class App extends Component {
         this.setState({term});
     }
 
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'nameStar':
+                return items.filter(item => item.nameStar);
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000);
+            default:
+                return items
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter});
+    }
+
     render() {
-        const {data, term} = this.state;
+        const {data, term, filter} = this.state;
         const employeers = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
-        const visibleDate = this.searchEmp(data, term);
+        const visibleDate = this.filterPost(this.searchEmp(data, term), filter);
 
         return (
             <div className="app">
@@ -90,7 +106,7 @@ class App extends Component {
 
                 <div className="search-panel">
                     <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
-                    <AppFilter/>
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
                 </div>
 
                 <EmployeersList data={visibleDate}
